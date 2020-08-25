281c92928bda99ba479ad262862f7333d9e7f211
bassyu <yrkdgus@gmail.com>
2020-08-25 19:14:42 +0900

    update

--- boj/14501.py
+++ boj/14501.py
@@ -1,6 +1,5 @@
-I = int
-l = [[*map(I, input().split())]for _ in range(I(input()))]
-d = [0]
+l = [[*map(int, input().split())]for i in range(int(input()))]
+dp = [0]
 for i in l[::-1]:
-    d += [max(I(i[0] > len(d))*(d[-i[0]]+i[1]), d[-1])]
-print(d[-1])
+    dp += [max(0 if i[0] > len(dp)else dp[-i[0]]+i[1], dp[-1])]
+print(dp[-1])
