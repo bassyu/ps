224a7088419cb75f86db7ec2c433b21e682380a3
bassyu <yrkdgus@gmail.com>
2020-09-06 18:57:19 +0900

    01927

--- boj/01927.py
+++ boj/01927.py
@@ -3,4 +3,4 @@ import heapq as h
 input()
 l = []
 for i in map(int, sys.stdin):
-    h.heappush(l, -i)if i else print(-h.heappop(l)if l else 0)
+    h.heappush(l, i)if i else print(h.heappop(l)if l else 0)
