4fcf5b9d0306cd474336ff9d9d1229b09f6692d0
bassyu <yrkdgus@gmail.com>
2020-09-08 03:25:29 +0900

    add 14888

---
+++ boj/14888.py
@@ -0,0 +1,16 @@
+from itertools import permutations
+
+N = int(input())
+A = list(map(int, input().split()))
+a, s, m, d = map(int, input().split())
+
+rs = list()
+for p in set(permutations('+'*a+'-'*s+'*'*m+'/'*d)):
+    r = A[0]
+    for i in range(1, N):
+        r = {'+': r+A[i], '-': r-A[i], '*': r*A[i], '/': int(r/A[i])}[p[i-1]]
+
+    rs.append(r)
+
+print(max(rs))
+print(min(rs))
