89b41e0860af8081c470db93999fe22e4cd5b482
bassyu <yrkdgus@gmail.com>
2020-09-15 00:57:35 +0900

    init leetcode

---
+++ leetcode/00001.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n in h:
+                return [h[n], i]
+            h[num] = i
