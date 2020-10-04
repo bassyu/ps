fd7d2850933d5d71f55cf5d92f4b7a1ccf63cbe1
bassyu <yrkdgus@gmail.com>
2020-10-04 22:36:38 +0900

    add 1929

---
+++ boj/01929.py
@@ -0,0 +1,10 @@
+l = [False, False] + [True]*1000000
+n_l = len(l)-1
+for i in range(2, n_l//2):
+    if l[i]:
+        l[i*2::i] = [False] * (n_l//i-1)
+
+M, N = map(int, input().split())
+l = [i for i in range(M, N+1) if l[i]]
+for i in l:
+    print(i)
