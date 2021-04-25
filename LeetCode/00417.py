77a54355ab4b774132638e94263238cea5091a92
GangHyun Yu <yrkdgus@gmail.com>
2021-04-25 20:18:09 +0900

    Update 00417.py

--- LeetCode/00417.py
+++ LeetCode/00417.py
@@ -1,20 +1,17 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        n, m = len(heights[0]), len(heights)
-        state = [[(False, False)] * n for _ in range(m)]
-        output = []
+        m, n = len(heights), len(heights[0])
+        p_visited, a_visited = set(), set()
+        def dfs(visited, x, y):
+            visited.add((y, x))
+            for xg, yg in [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]:
+                if xg in range(n) and yg in range(m) and (yg, xg) not in visited and heights[yg][xg] >= heights[y][x]:
+                    dfs(visited, xg, yg)
+                    
+        for i in range(n):
+            dfs(p_visited, i, 0)
+            dfs(a_visited, i, m-1)
         for i in range(m):
-            for j in range(n):
-                stack = [(j, i)]
-                while stack:
-                    x, y = stack.pop()
-                    s = state[y][x]
-                    state[y][x] = ((s[0] or x == 0 or y == 0),
-                                   (s[1] or x == n-1 or y == m-1))
-                    if all(s):
-                        output.append([x, y])
-                        break
-                    for xg, yg in [(x, y+1), (x, y-1), (x+1, y), (x-1, y)]:
-                        if xg in range(n) and yg in range(m) and heights[yg][xg] <= heights[y][x]:
-                            stack.append((xg, yg))
-        return output
+            dfs(p_visited, 0, i)
+            dfs(a_visited, n-1, i)
+        return list(p_visited.intersection(a_visited))
