ef364d654dd64e7e877f327e892f0a48d5b75fd9
Yu Ganghyeon <yrkdgus@gmail.com>
2022-02-09 20:11:18 +0900

    Update 3불량_사용자.py

--- programmers/3불량_사용자.py
+++ programmers/3불량_사용자.py
@@ -3,11 +3,11 @@ import re, itertools
 def solution(user_id, banned_id):
     size = len(banned_id)
     permutations = itertools.permutations(user_id, size)
-    patters = [re.compile(i.replace('*', '.')) for i in banned_id]
+    patterns = [re.compile(i.replace('*', '.')) for i in banned_id]
     
     s = set()
     for permutation in permutations:
-        l = [permutation[i] for i in range(size) if patters[i].fullmatch(permutation[i])]
+        l = [permutation[i] for i in range(size) if patterns[i].fullmatch(permutation[i])]
         if len(l) == size: 
             s.add(tuple(sorted(l)))
     
