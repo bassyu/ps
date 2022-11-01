cfaecfd9b0e06905c1ac489308a2c795aa2890a5
bassyu <yrkdgus@gmail.com>
2022-11-01 22:50:42 +0900

    Create 1콜라_문제.js

---
+++ programmers/1콜라_문제.js
@@ -0,0 +1,11 @@
+function solution(a, b, n) {
+    let count = 0;
+    while (n >= 2) {
+        const newCoke = parseInt(n / a) * b;
+        if (newCoke === 0) break;
+        count += newCoke;
+        n = n % a + newCoke;
+    }
+    
+    return count;
+}
