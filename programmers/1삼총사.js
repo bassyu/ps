e84f0e9f4d77f1512d951d0b94164d6a221e6c37
bassyu <yrkdgus@gmail.com>
2022-11-01 22:49:56 +0900

    Create 1삼총사.js

---
+++ programmers/1삼총사.js
@@ -0,0 +1,13 @@
+function solution(number) {
+    const l = number.length;
+    let count = 0;
+    for (let i = 0; i < l; i++) {
+        for (let j = i + 1; j < l; j++) {
+            for (let k = j + 1; k < l; k++) {
+                if (number[i] + number[j] + number[k] === 0) count += 1;
+            }
+        }
+    }
+    
+    return count;
+}
