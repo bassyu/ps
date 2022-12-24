13d3409144fae6416c732e0c755114dc84dc9fdc
bassyu <yrkdgus@gmail.com>
2022-12-24 23:42:33 +0900

    Edit 3_최고의_집합.js

--- programmers/3_최고의_집합.js
+++ programmers/3_최고의_집합.js
@@ -1,6 +1,6 @@
 function solution(n, s) {
   if (n > s) return [-1];
-  const set = new Array(n).fill(parseInt(s / n));
+  const set = new Array(n).fill(Math.floor(s / n));
   for (let i = 0; i < s % n; i += 1) {
     set[i] += 1;
   }
