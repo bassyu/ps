00e89103e0c7026d8541e31f77cacda4e57864e1
bassyu <yrkdgus@gmail.com>
2022-12-24 23:03:52 +0900

    Create 3_최고의_집합.js

---
+++ programmers/3_최고의_집합.js
@@ -0,0 +1,9 @@
+function solution(n, s) {
+  if (n > s) return [-1];
+  const set = new Array(n).fill(parseInt(s / n));
+  for (let i = 0; i < s % n; i += 1) {
+    set[i] += 1;
+  }
+
+  return set.sort((a, b) => a - b);
+}
